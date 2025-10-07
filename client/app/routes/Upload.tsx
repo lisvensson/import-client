184c70107834cfe6b07c110768ca8188cd067529
Li Svensson <lisvensson@msn.com>
2025-10-07 08:28:08 +0200

    added some styling to form and added parseCsvFile function

--- client/app/routes/Upload.tsx
+++ client/app/routes/Upload.tsx
@@ -1,6 +1,17 @@
 import { Form } from "react-router";
 import type { Route } from "./+types/Upload";
 
+export function parseCsvFile(content: string, fileName: string) {
+  const lines = content.trim().split("\n");
+  const rows = lines.map(line => line.trim().split(";"));
+  const dataRows = rows.slice(1);
+
+  return {
+    fileName,
+    rowCount: dataRows.length,
+  };
+}
+
 export async function action({ request }: Route.ActionArgs) {
   const formData = await request.formData();
   const file = formData.get("file");
@@ -14,21 +25,23 @@ export async function action({ request }: Route.ActionArgs) {
   }
 
   const fileContent = await file.text();
-  console.log("File content", fileContent);
+  const result = parseCsvFile(fileContent, file.name);
+  
+  console.log("Result: ", result);
 
-  return { success: true, fileContent: fileContent };
+  return { success: true, result: result };
 }
 
 export default function Upload({ actionData }: Route.ComponentProps) {
   return (
-    <div>
-      <h1>Ladda upp en CSV-fil</h1>
-      <Form method="post" encType="multipart/form-data" >
-        <input type="file" name="file" />
-        <button type="submit">Skicka</button>
+    <div className="max-w-md mx-auto mt-10 p-6 border border-gray-300">
+      <h1 className="text-center mb-6 text-2xl font-semibold">Ladda upp en CSV-fil</h1>
+      <Form method="post" encType="multipart/form-data" className="flex flex-col gap-4">
+        <input type="file" name="file" className="file:border file:border-gray-300 file:px-4 file:py-2 file:bg-gray-100 file:text-sm"/>
+        <button type="submit" className="bg-green-700 text-white py-2 hover:bg-green-900 cursor-pointer ">Skicka</button>
       </Form>
-      {actionData?.error && <p style={{ color: "red" }}>{actionData.error}</p>}
-      {actionData?.success && <p style={{ color: "green" }}>Uppladdning lyckades!</p>}
+      {actionData?.error && <p className="text-red-600 text-center mt-4">{actionData.error}</p>}
+      {actionData?.success && <p className="text-green-600 text-center mt-4">Uppladdning lyckades!</p>}
     </div>
   );
 }
